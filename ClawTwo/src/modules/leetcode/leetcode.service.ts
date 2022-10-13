64192f91766fbd0c273b8850a592fb5560af5d66
ClawInterspace <alanliu71104@gmail.com>
2022-10-14 00:08:22 +0800

    adopt nest native logger

--- ClawTwo/src/modules/leetcode/leetcode.service.ts
+++ ClawTwo/src/modules/leetcode/leetcode.service.ts
@@ -1,23 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import * as fs from 'fs';
 import * as path from 'path';
 import LeetcodeStatus from './dto/LeetcodeStatus';
 
 @Injectable()
 export class LeetcodeService {
+  private readonly logger = new Logger(LeetcodeService.name);
+
   getList(): LeetcodeStatus[] {
     const codeFolder = `C:\\Users\\s8711\\workspace\\learn\\leetcode-py`;
-    let allLeetcodeStatus: LeetcodeStatus[] = [];
+    const allLeetcodeStatus: LeetcodeStatus[] = [];
     fs.readdirSync(codeFolder).forEach((file) => {
       const p = path.join(codeFolder, file);
       if (fs.lstatSync(p).isFile()) {
-        console.log(path.parse(p).base);
+        this.logger.debug(path.parse(p).base);
         allLeetcodeStatus.push(
           new LeetcodeStatus({ name: path.parse(p).base }),
         );
       }
     });
-    // console.log(allLeetcodeFiles);
     return allLeetcodeStatus;
   }
 }

a2095c3c930d60cadd4901a7c03a24e50d6868c3
ClawInterspace <alanliu71104@gmail.com>
2022-10-17 13:46:00 +0800

    add config service

--- ClawTwo/src/app.module.ts
+++ ClawTwo/src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common'
 import { AppController } from './controllers/app.controller'
 import { AppService } from './app.service'
 import { LeetcodeModule } from './modules/leetcode/leetcode.module'
+import { ConfigModule } from '@nestjs/config'
+import { getEnvPath } from './config/envs/env.helper'
+
+const envFilePath: string = getEnvPath(`${__dirname}/config/envs`)
 
 @Module({
-  imports: [LeetcodeModule],
+  imports: [
+    ConfigModule.forRoot({ envFilePath, isGlobal: true }),
+    LeetcodeModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- ClawTwo/src/main.ts
+++ ClawTwo/src/main.ts
@@ -1,8 +1,17 @@
+import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { ConsoleLogger } from '@nestjs/common'
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule, { abortOnError: false })
-  await app.listen(3001)
+
+  const config: ConfigService = app.get(ConfigService)
+  const port: number = config.get<number>('PORT')
+  const logger: ConsoleLogger = new ConsoleLogger()
+
+  await app.listen(port, () => {
+    logger.log('[WEB]', config.get<string>('BASE_URL'))
+  })
 }
 bootstrap()

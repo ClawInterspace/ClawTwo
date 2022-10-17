f80eeb224e2bb4eca24fe99deacce379dc0f9789
ClawInterspace <alanliu71104@gmail.com>
2022-10-17 13:50:58 +0800

    remove redundant code

--- ClawTwo/src/app.service.ts
+++ ClawTwo/src/app.service.ts
@@ -1,11 +1,8 @@
 import { Injectable } from '@nestjs/common'
-import { LeetcodeService } from './modules/leetcode/leetcode.service'
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    const s = new LeetcodeService()
-    s.getList()
     return 'Hello ClawTwo!'
   }
 }
